68662f01b9021919e67ecd89ac64f4a00ddc5cb2
dani447eTietgen <dani447e@tietgen.dk>
2021-06-15 23:39:10 +0200

    258

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 
          <section class="section-default"><p class="login-status">Dine forslag</p></section>
                 <div class="grid">
-                  <div class="bil"> <img src="img/bil1.png" style="width: 100%; height: 180px;"> <p>Toyata mini |</p> <h1>85.000kr</h1> </div>
-                   <div class="bil"> <img src="img/bil2.jpeg" style="width: 100%; height: 180px;"> <p>Bentley ford |</p> <h1>365.000kr</h1> </div>
-                   <div class="bil"> <img src="img/bil3.jpeg" style="width: 100%; height: 180px;"> <p>Audi A8 |</p> <h1>200.000kr</h1></div>
-                   <div class="bil"> <img src="img/bil4.jpg" style="width: 100%; height: 180px;"> <p>Renault quick |</p> <h1>55.000kr</h1></div>
-                   <div class="bil"> <img src="img/bil5.jpeg" style="width: 100%; height: 180px;"> <p>Gamle Ole |</p> <h1>123.000kr</h1></div>
-                   <div class="bil"> <img src="img/bil6.jpg" style="width: 100%; height: 180px;"> <p>Mercedes future |</p> <h1>1.200.000kr</h1></div>
-                   <div class="bil"> <img src="img/bil7.jpg" style="width: 100%; height: 180px;"> <p>Pegeo.t 102 |</p> <h1>45.000kr</h1></div>
-                   <div class="bil"> <img src="img/bil8.jpg" style="width: 100%; height: 180px;"> <p>Skoda activa |</p> <h1>100.000kr</h1></div>
+                  <div class="bil"> <img src="img/bil1.png" style="width: 258px; height: 180px;"> <p>Toyata mini |</p> <h1>85.000kr</h1> </div>
+                   <div class="bil"> <img src="img/bil2.jpeg" style="width: 258px; height: 180px;"> <p>Bentley ford |</p> <h1>365.000kr</h1> </div>
+                   <div class="bil"> <img src="img/bil3.jpeg" style="width: 258px; height: 180px;"> <p>Audi A8 |</p> <h1>200.000kr</h1></div>
+                   <div class="bil"> <img src="img/bil4.jpg" style="width: 258px; height: 180px;"> <p>Renault quick |</p> <h1>55.000kr</h1></div>
+                   <div class="bil"> <img src="img/bil5.jpeg" style="width: 258px; height: 180px;"> <p>Gamle Ole |</p> <h1>123.000kr</h1></div>
+                   <div class="bil"> <img src="img/bil6.jpg" style="width: 258px; height: 180px;"> <p>Mercedes future |</p> <h1>1.200.000kr</h1></div>
+                   <div class="bil"> <img src="img/bil7.jpg" style="width: 258px; height: 180px;"> <p>Pegeo.t 102 |</p> <h1>45.000kr</h1></div>
+                   <div class="bil"> <img src="img/bil8.jpg" style="width: 258px; height: 180px;"> <p>Skoda activa |</p> <h1>100.000kr</h1></div>
                </div>
 
 
