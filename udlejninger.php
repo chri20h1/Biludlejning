6484af8830aff820d5e58478d786b0b9a34c4d2c
dani447eTietgen <dani447e@tietgen.dk>
2021-06-16 16:03:15 +0200

    udlej

--- udlejninger.php
+++ udlejninger.php
@@ -4,17 +4,16 @@
 
    <main>
      <div class="wrapper-main">
-       <section class="section-default">
          <?php if (isset($_SESSION['useruid'])) : ?>
 
-            <p class="login-status">Du er logget på!!!!</p>
+            <img src="img/udlejs.png" style="width: 100%; height: 100%;">
+
 
           <?php else : ?>
 
             <p class="login-status">Du er ikke logget på!</p>
 
            <?php endif; ?>
-       </section>
       </div>
      </main>
 <?php
