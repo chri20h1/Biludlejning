c5fce186b79bcaafa46083a506190df36a8e4bef
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2021-06-16 15:44:03 +0200

    push farfar

--- inventar.php
+++ inventar.php
@@ -4,10 +4,8 @@
 
    <main>
      <div class="wrapper-main">
-       <section class="section-default">
          <?php if (isset($_SESSION['useruid'])) : ?>
 
-            <p class="login-status">Du er logget på!!!!</p>
             <img src="img/Skitsebil.png" style="width: 100%; height: 100%;">
 
           <?php else : ?>
@@ -15,7 +13,6 @@
             <p class="login-status">Du er ikke logget på!</p>
 
            <?php endif; ?>
-       </section>
       </div>
      </main>
 <?php
